124ed99495594d00a84c30c16b81deb44432bf17
Deepika <deepikarthi527-tech>
2026-07-27 12:26:52 +0530

    Changes Done

--- welcome.java
+++ welcome.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Welcome {
+public class welcome {
 
     public static void main(String[] args) {
         System.out.println("Welcome to ABC Bank! How can we assist you today?");
