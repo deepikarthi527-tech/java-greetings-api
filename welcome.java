aa48d333c0ef2ffc61c1de19c92e32c0f19cfac5
deepikarthi527-tech <deepikarthi527@gmail.com>
2026-07-27 12:16:33 +0530

    Add Welcome class with greeting message

---
+++ welcome.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to ABC Bank! How can we assist you today?");
+    }
+}
